729233174513fdb1afe9b56849d34810a452c3e2
ChrisMunene <munenechristoph@gmail.com>
2017-02-24 13:04:00 +0300

    add all modified files

--- js/script.js
+++ js/script.js
@@ -1,37 +1,32 @@
 $(document).ready(function() {
     $("form#pingpong").submit(function(event) {
-        $("#result").empty();
+        event.preventDefault();
         var number = parseInt($("input#userInput").val());
-        var result = createGame(number);
-        result.forEach(function(out){
-            $("#result").append("<li>" + out + "</li>");
-            event.preventDefault();
-        });
-
-
-function createGame(number) {
-
 
     for (var index = 1; index <= number; index++) {
 
         var out = [];
 
-        if ((index % 5) === 0 && (index % 3)=== 0) {
+        if (index % 15===0)  {
 
             out.push("pingpong");
 
-        } else if ((index % 5) === 0) {
+        } else if (index % 5 === 0) {
 
             out.push("pong");
-        } else if ((index % 3)=== 0) {
+        } else if (index % 3=== 0) {
             out.push("ping");
 
         } else {
             out.push(index);
 
         }
-    }
-    return out;
+
+  };
+    for ( var ind=0;ind<index;ind++){
+      var output = out[ind]
+      $("#result").append("<li>"+output+"</li>")
+
 };
 
     });
