2e9f963342d6fa83c5216ec56a68e6768f6d9c62
ChrisMunene <munenechristoph@gmail.com>
2017-03-13 17:27:06 +0300

    add interface js

--- js/script.js
+++ js/script.js
@@ -1,25 +1,15 @@
-
-$(document).ready(function() {
-    $("#pingpong").submit(function(event) {
-        event.preventDefault();
-        var number = parseInt($("#userInput").val());
-
-        var output = [];
-
-        for (var index = 1; index <= number; index++) {
-            if ((index % 15)=== 0) {
-                output.push("pingpong");
-            } else if ((index % 5) === 0) {
-                output.push("pong");
-            } else if ((index % 3) === 0) {
-                output.push("ping");
-            } else {
-                output.push(index);
-            }
-        };
-        $("#result").empty();
-        output.forEach(function(element) {
-            $("#result").append("<li>" + element + "</li>");
-        });
-    });
-});
+function pingPong(goal) {
+    var output = [];
+    for (var index = 1; index <= goal; index++) {
+        if ((index % 15) === 0) {
+            output.push("pingpong");
+        } else if ((index % 5) === 0) {
+            output.push("pong");
+        } else if ((index % 3) === 0) {
+            output.push("ping");
+        } else {
+            output.push(index);
+        }
+    };
+    return output;
+}
